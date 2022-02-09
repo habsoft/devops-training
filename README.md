cf7b1c23c73988e782126b20f850a0e4e79f5cc3
GouseAli <gouseali30@gmail.com>
2022-02-09 21:58:43 +0500

    Added S3 Static-Website Terraform

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 1. Terraform
 2. Ansible
 3. AWS CDK
+
+## Pre Requisites
+## Deployment Steps
---
+++ terraform/s3-static-website/iam.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+data "aws_iam_policy_document" "website_policy" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    principals {
+      identifiers = ["*"]
+      type = "AWS"
+    }
+    resources = [
+      "arn:aws:s3:::mys3.terraform/*"
+    ]
+  }
+}
